e7f403ed4b6885e7e6194bec0a41ec53ec78d718
shinji <cpf2015@gmail.com>
2022-01-04 10:25:36 +0800

    fix: import extensions

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -9,15 +9,23 @@ module.exports = {
     'plugin:import/recommended',
     'plugin:eslint-comments/recommended',
   ],
+  settings: {
+    'import/resolver': {
+      node: { extensions: ['.js', '.mjs', '.ts', '.d.ts'] },
+    },
+  },
   rules: {
     // import
     'import/no-unresolved': 'off',
-    'import/extensions': ['error', {
-      js: 'never',
-      jsx: 'never',
-      ts: 'never',
-      tsx: 'never',
-    }],
+    'import/extensions': ['error', 'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
 
     // comments
     'eslint-comments/disable-enable-pair': 'off',
